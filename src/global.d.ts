b05b87438aaa4391ed8e2e3d4fd1c5e6e7f73c55
Luis Alvarez <luisito453@gmail.com>
2018-05-14 12:58:32 -0500

    Removed global.d.ts, no longer required with new version of react-apollo

--- src/global.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * react-apollo@^2.0.0 has an issue with lodash types
- * https://github.com/apollographql/react-apollo/issues/1286
- *
- * although it's fixed with react-apollo beta, adding it may not be the best
- * idea and it's also not compatible with next
- * https://github.com/apollographql/react-apollo/issues/1591
- */
-declare module 'lodash.flowright';
