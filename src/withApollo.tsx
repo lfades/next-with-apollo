3021d82bdc8447176d2219add67b5202975953cf
Dacio Romero <dacioromero@gmail.com>
2019-08-11 21:19:06 -0700

    Use @apollo/react-ssr

--- src/withApollo.tsx
+++ src/withApollo.tsx
@@ -1,9 +1,9 @@
+import { getDataFromTree } from '@apollo/react-ssr';
 import ApolloClient from 'apollo-client';
 import { AppProps, default as NextApp } from 'next/app';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { getDataFromTree } from 'react-apollo';
 import initApollo from './apollo';
 import {
   ApolloContext,
